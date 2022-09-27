80b226d0cbd36083bc814517df5171a8e9151c4c
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2022-09-27 09:50:39 -0500

    Validaciones manuales

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,23 +1,40 @@
 const express = require('express')
 
-const loginUsuario = (req, res) => {
+const loginUsuario = (req, res = express.response) => {
+
+  const { email, password } = req.body
 
   res.json({
     ok: true,
-    msg: 'login'
+    msg: 'login',
+    email,
+    password
   })
 
 }
 
 const crearUsuario = (req, res = express.response)=>{
 
-  res.json({
+  const { name, email, password } = req.body
+
+  
+  if (name.length < 5){
+    return res.status(400).json({
+      ok: false,
+      msg: 'El nombre debe tener más de 5 carácteres'
+    })
+  }
+
+  res.status(201).json({
     ok: true,
-    msg: 'registro'
+    msg: 'registro',
+    name,
+    email,
+    password,
   })
 }
 
-const revalidarToken = (req, res)=>{
+const revalidarToken = (req, res = express.response)=>{
 
   res.json({
     ok: true,
