bf9146bf012330657d874ece708ef9a33a58b70c
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2022-09-28 08:31:29 -0500

    Agregar evento a BD

--- routes/events.js
+++ routes/events.js
@@ -6,6 +6,9 @@
 const { Router } = require("express");
 const { validarJWT } = require('../middlewares/validar-jwt')
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require("../controllers/events");
+const { check } = require('express-validator')
+const { validarCampos } = require('../middlewares/validar-campos');
+const isDate = require("../helpers/isDate");
 const router = Router()
 
 // Todas tienen que pasar por la validación del JWT
@@ -15,7 +18,12 @@ router.use(validarJWT)
 router.get('/', getEventos)
 
 // Crear un nuevo evento
-router.post('/', crearEvento)
+router.post('/', [
+  check('title', 'El titulo es obligatorio').not().isEmpty(),
+  check('start', 'La fecha de inicio es obligatoria').custom(isDate),
+  check('end', 'La fecha de finalización es obligatoria').custom(isDate),
+  validarCampos
+], crearEvento)
 
 // Actualizar evento
 router.put('/:id', actualizarEvento)
