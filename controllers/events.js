ab9f4b7aead09129cb524a06e03bdcf9bcd5478c
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2022-09-28 09:45:36 -0500

    Eliminar evento

--- controllers/events.js
+++ controllers/events.js
@@ -79,11 +79,45 @@ const actualizarEvento = async (req, res = response) => {
 
 }
 
-const eliminarEvento = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: 'Eliminar evento'
-  })
+const eliminarEvento = async (req, res = response) => {
+  
+  const eventoId = req.params.id
+  const uid = req.uid
+
+  try {
+    const evento = await Evento.findById(eventoId)
+
+    if (!evento){
+      return res.status(404).json({
+        ok: false,
+        mgs: 'No existe un evento con ese ID'
+      })
+    }
+
+    if (evento.user.toString() !== uid){
+      return res.status(401).json({
+        ok: false,
+        msg: 'No tiene privilegio de editar este objeto'
+      })
+    }
+
+    const eventoEliminado = await Evento.findByIdAndRemove(eventoId)
+
+    res.status(200).json({
+      ok: true,
+      evento: eventoEliminado
+    })
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador'
+    })
+  }
+
+
+
 }
 
 // {
